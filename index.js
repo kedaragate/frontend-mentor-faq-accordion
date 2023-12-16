17fd9002827d976b865efdae566b05ce0cf71bdc
Kedar Agate <kedar.agate@gmail.com>
2023-12-16 19:55:21 +0530

    completed

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const minusIcon = document.querySelector(".minus-icon");
 const faqs = document.querySelectorAll(".faq");
 const answer = document.querySelector(".answer");
 faqs.forEach((faq) => {
-  faq.addEventListener("click", (e) => {
+  faq.addEventListener("click", () => {
+    faqs.forEach((que) => {
+      que.querySelector(".answer").classList.add("hidden");
+      que.querySelector(".plus-icon").classList.remove("hidden");
+      que.querySelector(".minus-icon").classList.add("hidden");
+    });
     faq.querySelector(".answer").classList.toggle("hidden");
     faq.querySelector(".plus-icon").classList.toggle("hidden");
     faq.querySelector(".minus-icon").classList.toggle("hidden");
